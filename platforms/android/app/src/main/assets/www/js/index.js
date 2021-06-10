c735e2ecfdaa524787e339149fc48c84111906a7
Daniel <danistark29@gmail.com>
2021-06-10 11:55:26 -0500

    Error input email,  phone y genderr

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -78,7 +78,7 @@ window.showResult = result => {
     const resultElement = document.createElement('div');
     resultElement.id = "result";
     resultElement.classList = "result";
-    // resultElement.innerHTML = `<p>${result}</p><button onclick="continueScanning()">OK</button>`;
+
     document.querySelector('#formulario').appendChild(resultElement);
     console.log('Resultado', result)
     document.getElementById("nombre").value = `${result.firstName || "empty"}`;
@@ -89,49 +89,100 @@ window.showResult = result => {
     document.getElementById("label-apellido-materno").className = "label active";
     document.getElementById("pasaporte").value = `${result.documentNumber || "empty"}`;
     document.getElementById("label-pasaporte").className = "label active";
-    /* document.getElementById("genero").value = `${result.sex || "empty"}`; */
-    /* document.getElementById("label-genero").className = "label active"; */
+
     document.getElementById("nacionalidad").value = `${result.issuingCountry || "empty"}`;
     document.getElementById("label-nacionalidad").className = "label active";
     let fcadena = {}
     fcadena = result.dateOfBirth.json
     if (fcadena.month < 10) { fcadena.month = "0" + fcadena.month.toString() }
     if (fcadena.day < 10) { fcadena.day = "0" + fcadena.day.toString() }
-    /* let fecha = Object.values(result.dateOfBirth.json).reverse().join().replace(/,/g, "-") */
+
     let fecha = Object.values(fcadena).reverse().join().replace(/,/g, "-")
-    console.log('fecha', fecha);
+
     document.getElementById("nacimiento").value = `${fecha || "empty"}`;
     document.getElementById("label-nacimiento").className = "label active";
 }
+
+
+// window.sendData = () => {
+//     document.getElementById("spinner").style.display = "block";
+//     document.getElementById("formulario").style.display = "none";
+
+//     const lastName = document.getElementById("apellido-paterno").value;
+//     const name = document.getElementById("apellido-paterno").value;
+//     const MlastName = document.getElementById("apellido-materno").value;
+//     const id = document.getElementById("pasaporte").value;
+//     const gender = document.getElementById("genero").value;
+//     const nationality = document.getElementById("nacionalidad").value;
+//     const date = document.getElementById("nacimiento").value;
+//     const email = document.getElementById("correo").value;
+//     const phone = document.getElementById("telefono").value;
+
+//     setTimeout(function() {
+//         document.getElementById("spinner").style.display = "none";
+
+//         const options = {
+//             method: 'post',
+//             data: {
+//                 action: "registrar",
+//                 lastName: lastName,
+//                 name: name,
+//                 MlastName: MlastName,
+//                 id: id,
+//                 gender: gender,
+//                 nationality: nationality,
+//                 date: date,
+//                 email: email,
+//                 phone: phone
+//             },
+//             headers: { urlencoded: "application/x-www-form-urlencoded" }
+//         };
+
+//         cordova.plugin.http.sendRequest('https://microlab.mx/amatis21qroo/interfaces/registromovil.php', options, function(response) {
+//             // prints 200
+//             console.log(response.status);
+//         }, function(response) {
+//             // prints 403
+//             console.log(response.status);
+
+//             //prints Permission denied
+//             console.log(response.error);
+//         });
+
+
+//         document.getElementById("data-capture-view").style.display = "block";
+//         window.idCapture.isEnabled = true;
+//     }, 5000);
+
+// }
 
 window.sendData = () => {
     document.getElementById("spinner").style.display = "block";
     document.getElementById("formulario").style.display = "none";
 
     setTimeout(function() {
-        document.getElementById("spinner").style.display = "none";
 
         const options = {
             method: 'post',
             data: {
-                    action: "registrar",
-                    lastName: "Lopez",
-                    name: "Daniel3",
-                    MlastName: "Lopez",
-                    id: "78787878",
-                    gender: "female",
-                    nationality: "mex",
-                    date: "1995-05-27",
-                    email: "daniel3@gmail.com",
-                    phone: "45455476848"
-                },
+                action: "registrar",
+                lastName: document.getElementById("apellido-paterno").value,
+                name: document.getElementById("nombre").valu,
+                MlastName: document.getElementById("apellido-materno").value,
+                id: document.getElementById("pasaporte").value,
+                gender: document.getElementById("genero").value,
+                nationality: document.getElementById("nacionalidad").value,
+                date: document.getElementById("nacimiento").value,
+                email: "ejemplo@correo.com",
+                phone: "4778484884"
+            },
             headers: { urlencoded: "application/x-www-form-urlencoded" }
-            };
+        };
 
-            cordova.plugin.http.sendRequest('https://microlab.mx/amatis21qroo/interfaces/registromovil.php', options, function(response) {
+        cordova.plugin.http.sendRequest('https://microlab.mx/amatis21qroo/interfaces/registromovil.php', options, function(response) {
             // prints 200
             console.log(response.status);
-            }, function(response) {
+        }, function(response) {
             // prints 403
             console.log(response.status);
 
@@ -141,7 +192,7 @@ window.sendData = () => {
 
         document.getElementById("data-capture-view").style.display = "block";
         window.idCapture.isEnabled = true;
-    }, 5000);
+    }, 7000);
 }
 
 window.continueScanning = () => {
@@ -155,129 +206,4 @@ window.continueScanning = () => {
         window.idCapture.isEnabled = true;
     }, 3000);
 
-}
-
-// === //
-
-window.descriptionForMrzResult = (result) => {
-    return `
-  ${descriptionForCapturedId(result)}<br><br>
-  Document Code: ${result.mrzResult.documentCode}<br>
-  Names Are Truncated: ${result.mrzResult.namesAreTruncated ? "Yes" : "No"}<br>
-  Optional: ${result.mrzResult.optional || "empty"}<br>
-  Optional 1: ${result.mrzResult.optional1 || "empty"}<br>
-  `
-}
-
-window.descriptionForVizResult = (result) => {
-    return `
-  ${descriptionForCapturedId(result)}<br><br>
-  Additional Name Information: ${result.vizResult.additionalNameInformation || "empty"}<br>
-  Additional Address Information: ${result.vizResult.additionalAddressInformation || "empty"}<br>
-  Place of Birth: ${result.vizResult.placeOfBirth || "empty"}<br>
-  Race: ${result.vizResult.race || "empty"}<br>
-  Religion: ${result.vizResult.religion || "empty"}<br>
-  Profession: ${result.vizResult.profession || "empty"}<br>
-  Marital Status: ${result.vizResult.maritalStatus || "empty"}<br>
-  Residential Status: ${result.vizResult.residentialStatus || "empty"}<br>
-  Employer: ${result.vizResult.employer || "empty"}<br>
-  Personal Id Number: ${result.vizResult.personalIdNumber || "empty"}<br>
-  Document Additional Number: ${result.vizResult.documentAdditionalNumber || "empty"}<br>
-  Issuing Jurisdiction: ${result.vizResult.issuingJurisdiction || "empty"}<br>
-  Issuing Authority: ${result.vizResult.issuingAuthority || "empty"}<br>
-  `
-}
-
-window.descriptionForUsDriverLicenseBarcodeResult = (result) => {
-    return `
-  ${descriptionForCapturedId(result)}<br><br>
-  AAMVA Version: ${result.aamvaBarcodeResult.aamvaVersion}<br>
-  Jurisdiction Version: ${result.aamvaBarcodeResult.jurisdictionVersion}<br>
-  IIN: ${result.aamvaBarcodeResult.iIN}<br>
-  Issuing Jurisdiction: ${result.aamvaBarcodeResult.issuingJurisdiction}<br>
-  Issuing Jurisdiction ISO: ${result.aamvaBarcodeResult.issuingJurisdictionISO}<br>
-  Eye Color: ${result.aamvaBarcodeResult.eyeColor || "empty"}<br>
-  Hair Color: ${result.aamvaBarcodeResult.hairColor || "empty"}<br>
-  Height Inch: ${result.aamvaBarcodeResult.heightInch || 0}<br>
-  Height Cm: ${result.aamvaBarcodeResult.heightCm || 0}<br>
-  Weight Lb: ${result.aamvaBarcodeResult.weightLbs || 0}<br>
-  Weight Kg: ${result.aamvaBarcodeResult.weightKg || 0}<br>
-  Place of Birth: ${result.aamvaBarcodeResult.placeOfBirth || "empty"}<br>
-  Race: ${result.aamvaBarcodeResult.race || "empty"}<br>
-  Document Discriminator Number: ${result.aamvaBarcodeResult.documentDiscriminatorNumber || "empty"}<br>
-  Vehicle Class: ${result.aamvaBarcodeResult.vehicleClass || "empty"}<br>
-  Restrictions Code: ${result.aamvaBarcodeResult.restrictionsCode || "empty"}<br>
-  Endorsements Code: ${result.aamvaBarcodeResult.endorsementsCode || "empty"}<br>
-  Card Revision Date: ${JSON.stringify(result.aamvaBarcodeResult.cardRevisionDate.date) || "empty"}<br>
-  Middle Name: ${result.aamvaBarcodeResult.middleName || "empty"}<br>
-  Driver Name Suffix: ${result.aamvaBarcodeResult.driverNameSuffix || "empty"}<br>
-  Driver Name Prefix: ${result.aamvaBarcodeResult.driverNamePrefix || "empty"}<br>
-  Last Name Truncation: ${result.aamvaBarcodeResult.lastNameTruncation || "empty"}<br>
-  First Name Truncation: ${result.aamvaBarcodeResult.firstNameTruncation || "empty"}<br>
-  Middle Name Truncation: ${result.aamvaBarcodeResult.middleNameTruncation || "empty"}<br>
-  Alias Family Name: ${result.aamvaBarcodeResult.aliasFamilyName || "empty"}<br>
-  Alias Given Name: ${result.aamvaBarcodeResult.aliasGivenName || "empty"}<br>
-  Alias Suffix Name: ${result.aamvaBarcodeResult.aliasSuffixName || "empty"}<br>
-  `
-}
-
-window.descriptionForUsUniformedServicesBarcodeResult = (result) => {
-    return `
-  ${descriptionForCapturedId(result)}<br><br>
-  Version: ${result.usUniformedServicesBarcodeResult.version}<br>
-  Sponsor Flag: ${result.usUniformedServicesBarcodeResult.sponsorFlag}<br>
-  Person Designator Document: ${result.usUniformedServicesBarcodeResult.personDesignatorDocument}<br>
-  Family Sequence Number: ${result.usUniformedServicesBarcodeResult.familySequenceNumber}<br>
-  Deers Dependent Suffix Code: ${result.usUniformedServicesBarcodeResult.deersDependentSuffixCode}<br>
-  Deers Dependent Suffix Description: ${result.usUniformedServicesBarcodeResult.deersDependentSuffixDescription}<br>
-  Height: ${result.usUniformedServicesBarcodeResult.height}<br>
-  Weight: ${result.usUniformedServicesBarcodeResult.weight}<br>
-  Hair Color: ${result.usUniformedServicesBarcodeResult.hairColor}<br>
-  Eye Color: ${result.usUniformedServicesBarcodeResult.eyeColor}<br>
-  Direct Care Flag Code: ${result.usUniformedServicesBarcodeResult.directCareFlagCode}<br>
-  Direct Care Flag Description: ${result.usUniformedServicesBarcodeResult.directCareFlagDescription}<br>
-  Civilian Health Care Flag Code: ${result.usUniformedServicesBarcodeResult.civilianHealthCareFlagCode}<br>
-  Civilian Health Care Flag Description: ${result.usUniformedServicesBarcodeResult.civilianHealthCareFlagDescription}<br>
-  Commissary Flag Code: ${result.usUniformedServicesBarcodeResult.commissaryFlagCode}<br>
-  Commissary Flag Description: ${result.usUniformedServicesBarcodeResult.commissaryFlagDescription}<br>
-  MWR Flag Code: ${result.usUniformedServicesBarcodeResult.mwrFlagCode}<br>
-  MWR Flag Description: ${result.usUniformedServicesBarcodeResult.mwrFlagDescription}<br>
-  Exchange Flag Code: ${result.usUniformedServicesBarcodeResult.exchangeFlagCode}<br>
-  Exchange Flag Description: ${result.usUniformedServicesBarcodeResult.exchangeFlagDescription}<br>
-  Champus Effective Date: ${JSON.stringify(result.usUniformedServicesBarcodeResult.champusEffectiveDate.date) || "empty"}<br>
-  Champus Expiry Date: ${JSON.stringify(result.usUniformedServicesBarcodeResult.champusExpiryDate.date) || "empty"}<br>
-  Form Number: ${result.usUniformedServicesBarcodeResult.formNumber}<br>
-  Security Code: ${result.usUniformedServicesBarcodeResult.securityCode}<br>
-  Service Code: ${result.usUniformedServicesBarcodeResult.serviceCode}<br>
-  Status Code: ${result.usUniformedServicesBarcodeResult.statusCode}<br>
-  Status Code Description: ${result.usUniformedServicesBarcodeResult.statusCodeDescription}<br>
-  Branch Of Service: ${result.usUniformedServicesBarcodeResult.branchOfService}<br>
-  Rank: ${result.usUniformedServicesBarcodeResult.rank}<br>
-  Pay Grade: ${result.usUniformedServicesBarcodeResult.payGrade}<br>
-  Sponsor Name: ${result.usUniformedServicesBarcodeResult.sponsorName || "empty"}<br>
-  Sponsor Person Designator Identifier: ${result.usUniformedServicesBarcodeResult.sponsorPersonDesignatorIdentifier || 0}<br>
-  Relationship Code: ${result.usUniformedServicesBarcodeResult.relationshipCode || "empty"}<br>
-  Relationship Description: ${result.usUniformedServicesBarcodeResult.relationshipDescription || "empty"}<br>
-  Geneva Convention Category: ${result.usUniformedServicesBarcodeResult.genevaConventionCategory || "empty"}<br>
-  Blood Type: ${result.usUniformedServicesBarcodeResult.bloodType || "empty"}<br>
-  `
-}
-
-window.descriptionForCapturedId = (result) => {
-    return `
-  Name: ${result.firstName || "empty"}<br>
-  Last Name: ${result.lastName || "empty"}<br>
-  Full Name: ${result.fullName}<br>
-  Sex: ${result.sex || "empty"}<br>
-  Date of Birth: ${JSON.stringify(result.dateOfBirth && result.dateOfBirth.date) || "empty"}<br>
-  Nationality: ${result.nationality || "empty"}<br>
-  Address: ${result.address || "empty"}<br>
-  Document Type: ${result.documentType}<br>
-  Captured Result Type: ${result.capturedResultType}<br>
-  Issuing Country: ${result.issuingCountry || "empty"}<br>
-  Issuing Country ISO: ${result.issuingCountryISO || "empty"}<br>
-  Document Number: ${result.documentNumber || "empty"}<br>
-  Date of Expiry: ${JSON.stringify(result.dateOfExpiry && result.dateOfExpiry.date) || "empty"}<br>
-  Date of Issue: ${JSON.stringify(result.dateOfIssue && result.dateOfIssue.date) || "empty"}<br>
-  `
 }
